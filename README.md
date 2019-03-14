e4e171b365df041eeb31358677fefae9ef591e2d
Giovanni <g.salmeri@mondodomani.org>
2019-03-14 12:08:50 +0100

    Textual addresses via nominatim, code refactored

--- README.md
+++ README.md
@@ -18,22 +18,35 @@ Create a `[openstreetmap]` shortcut.
 
 The following arguments are available, all but the first argument are optional:
 
-`Coordinates` = geo URI (i.e. latitude and longitude separated with a `,` without spaces, optionally prefixed with the scheme `geo:`)  
+`Address` = either textual address (wrap multiple words into quotes) or geo URI (i.e. coordinates separated with a `,` without spaces, optionally prefixed with the scheme `geo:`)  
 `Zoom` = zoom value from 0 to 19, the default zoom is 15  
 `Style` = map style, e.g. `left`, `center`, `right`; the default style is `center`  
 `Width` = map width, pixel or percent; the deafult is 450  
 `Height` = map height, pixel; the default is 450  
 `Layer` = map layer: you can choose between `standard`, `cycle`, `transport`, `humanitarian` (see [explication](https://wiki.openstreetmap.org/wiki/Browsing#Layers)); append `+marker` (e.g. `standard+marker`) to add the marker; the default is `standard`  
 
-For getting the exact coordinates, go to [openstreetmap.org](https://www.openstreetmap.org/) and enter the address; the coordinates are the last numbers in the URL shown in the browser (if the URL `https://www.openstreetmap.org/#map=17/41.85181/12.62127` the coordinates are `41.85181,12.62127`). To be as precise as possible, select the *Share* icon on the right, select *Include marker*, and drag the marker onto the point that will be the center of the embedded map.
+Geo URIs make the maps load faster and allow a greater precision. For getting the exact coordinates, go to [openstreetmap.org](https://www.openstreetmap.org/) and enter the address; the coordinates are the last numbers in the URL shown in the browser (if the URL `https://www.openstreetmap.org/#map=17/41.85181/12.62127` the coordinates are `41.85181,12.62127`). To be as precise as possible, select the *Share* icon on the right, select *Include marker*, and drag the marker onto the point that will be the center of the embedded map.
+
+Textual addresses require a valid email in the configuration of the site in `system/settings/system.ini`.
+
+## How to configure OpenStreetMap
+
+The following settings can be configured in file `system/settings/system.ini`.
+
+`openstreetmapZoom` (default:  `15`) = default zoom  
+`openstreetmapStyle` (default:  `center`) = default style  
+`openstreetmapWidth` (default:  `450`) = default width  
+`openstreetmapHeight` (default:  `450`) = default height  
+`openstreetmapLayer` (default:  `standard`) = default layer  
 
 ## Example
 
 Embedding a map:
 
     [openstreetmap 41.85181,12.62127]
+    [openstreetmap "Via Columbia 1 Roma"]
     [openstreetmap 41.85181,12.62127 17 center 600 400 standard+marker]
-    [openstreetmap geo:41.85181,12.62127 17 center 100% 400 transport]
+    [openstreetmap 41.85181,12.62127 17 center 100% 400 transport]
 
 ## Developer
 
--- openstreetmap.php
+++ openstreetmap.php
@@ -1,5 +1,5 @@
 <?php
-// OpenStreetMap plugin
+// OpenStreetMap extension
 // Copyright (c) 2019 Giovanni Salmeri
 // This file may be used and distributed under the terms of the public license.
 
@@ -11,41 +11,17 @@ class YellowOpenStreetMap {
     // Handle initialisation
     public function onLoad($yellow) {
         $this->yellow = $yellow;
-        $this->yellow->system->setDefault("openstreetmapWidth", "450");
-        $this->yellow->system->setDefault("openstreetmapHeight", "450");
         $this->yellow->system->setDefault("openstreetmapZoom", "15");
         $this->yellow->system->setDefault("openstreetmapStyle", "center");
+        $this->yellow->system->setDefault("openstreetmapWidth", "450");
+        $this->yellow->system->setDefault("openstreetmapHeight", "450");
         $this->yellow->system->setDefault("openstreetmapLayer", "standard");
-
-        // https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
-        function getTileNumber($lat, $lon, $zoom) {
-           $xtile = (($lon + 180.0) / 360.0) * pow(2, $zoom); // no rounding with floor()
-           $ytile = (1 - log(tan(deg2rad($lat)) + 1 / cos(deg2rad($lat))) / pi()) /2 * pow(2, $zoom);
-           return array($xtile, $ytile);
-        }
-        function getCoord($xtile, $ytile, $zoom) {
-           $n = pow(2, $zoom);
-           $lon_deg = $xtile / $n * 360.0 - 180.0;
-           $lat_deg = rad2deg(atan(sinh(pi() * (1 - 2 * $ytile / $n))));
-           return array($lon_deg, $lat_deg);
-        }
-        function coordToBbox($lat, $lon, $zoom, $width, $height) {
-           define(TILE_SIZE, 256);
-           list($xtile, $ytile) = getTileNumber($lat, $lon, $zoom);
-           $xtile_s = ($xtile * TILE_SIZE - $width/2) / TILE_SIZE;
-           $ytile_s = ($ytile * TILE_SIZE - $height/2) / TILE_SIZE;
-           $xtile_e = ($xtile * TILE_SIZE + $width/2) / TILE_SIZE;
-           $ytile_e = ($ytile * TILE_SIZE + $height/2) / TILE_SIZE;
-           list($lon_s, $lat_s) = getCoord($xtile_s, $ytile_s, $zoom);
-           list($lon_e, $lat_e) = getCoord($xtile_e, $ytile_e, $zoom);
-           return "$lon_s,$lat_s,$lon_e,$lat_e";
-        }
     }
     
     // Handle page content parsing of custom block
-    public function onParseContentShortcut($page, $name, $text, $shortcut) {
+    public function onParseContentShortcut($page, $name, $text, $type) {
         $output = null;
-        if ($name=="openstreetmap" && $shortcut) {
+        if ($name=="openstreetmap" && ($type=="block" || $type=="inline")) {
             $layers = [
                 'standard' => 'mapnik',
                 'transport' => 'transportmap',
@@ -58,13 +34,17 @@ class YellowOpenStreetMap {
             if (empty($zoom)) $zoom = $this->yellow->system->get("openstreetmapZoom");
             if (empty($style)) $style = $this->yellow->system->get("openstreetmapStyle");
             if (empty($layer)) $style = $this->yellow->system->get("openstreetmapLayer");
+
             if (substr($address, 0, 4) == "geo:") $address = (substr($address, 4));
             list($lat, $lon) = explode(",", explode(";", $address)[0]);
+            if (!is_numeric($lat) || !is_numeric($lon)) list($lat, $lon) = $this->geolocation($address);
+
             list($layer, $marker) = explode("+", $layer);
             $layer = $layers[$layer];
-            $bbox = coordToBbox($lat, $lon, $zoom, (is_numeric($width) ? $width : 1), $height);
+
+            $bbox = $this->coordToBbox($lat, $lon, $zoom, (is_numeric($width) ? $width : 1), $height);
             $output = "<div class=\"".htmlspecialchars($style)."\">";
-            $output .= "<iframe src=\"https://www.openstreetmap.org/export/embed.html?bbox=".rawurlencode($bbox)."&amp;layer=".rawurlencode($layer);
+            $output .= "<iframe src=\"https://www.openstreetmap.org/export/embed.html?bbox=".rawurlencode($bbox)."&amp;layer=".$layer;
             if ($marker == "marker") $output .= "&amp;marker=".rawurlencode("$lat,$lon");
             $output .= "\" frameborder=\"0\"";
             if ($width && $height) $output .= " width=\"".htmlspecialchars($width)."\" height=\"".htmlspecialchars($height)."\"";
@@ -74,4 +54,37 @@ class YellowOpenStreetMap {
         return $output;
     }
 
+    // https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
+    function getTileNumber($lat, $lon, $zoom) {
+       $xtile = (($lon + 180) / 360) * pow(2, $zoom); // no rounding with floor()
+       $ytile = (1 - log(tan(deg2rad($lat)) + 1 / cos(deg2rad($lat))) / pi()) /2 * pow(2, $zoom);
+       return array($xtile, $ytile);
+    }
+    function getCoord($xtile, $ytile, $zoom) {
+       $n = pow(2, $zoom);
+       $lat_deg = rad2deg(atan(sinh(pi() * (1 - 2 * $ytile / $n))));
+       $lon_deg = $xtile / $n * 360 - 180;
+       return array($lat_deg, $lon_deg);
+    }
+    function coordToBbox($lat, $lon, $zoom, $width, $height) {
+       define(TILE_SIZE, 256);
+       list($xtile, $ytile) = $this->getTileNumber($lat, $lon, $zoom);
+       $xtile_s = ($xtile * TILE_SIZE - $width/2) / TILE_SIZE;
+       $ytile_s = ($ytile * TILE_SIZE - $height/2) / TILE_SIZE;
+       $xtile_e = ($xtile * TILE_SIZE + $width/2) / TILE_SIZE;
+       $ytile_e = ($ytile * TILE_SIZE + $height/2) / TILE_SIZE;
+       list($lat_s, $lon_s) = $this->getCoord($xtile_s, $ytile_s, $zoom);
+       list($lat_e, $lon_e) = $this->getCoord($xtile_e, $ytile_e, $zoom);
+       return "$lon_s,$lat_s,$lon_e,$lat_e";
+    }
+    // https://wiki.openstreetmap.org/wiki/Nominatim#Address_lookup
+    function geolocation($address) {
+        $email = $this->yellow->system->get("email");
+        $nominatim = simplexml_load_file("https://nominatim.openstreetmap.org/search?format=xml&email=$email&q=$address");
+        if ($nominatim) {
+            $lat = $nominatim->place["lat"];
+            $lon = $nominatim->place["lon"];
+            return array((float)$lat, (float)$lon);
+        }
+    }
 }
